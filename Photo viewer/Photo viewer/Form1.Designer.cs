8166dd3befdc70af392b80a0e93b6ecbc425f88c
MrKevinLeeTW <38918101+MrKevinLeeTW@users.noreply.github.com>
2018-11-03 18:11:00 +0800

    Add files via upload

---
+++ Photo viewer/Photo viewer/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Photo_viewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.right = new System.Windows.Forms.Button();
+            this.automatic = new System.Windows.Forms.Button();
+            this.left = new System.Windows.Forms.Button();
+            this.select = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // right
+            // 
+            this.right.Location = new System.Drawing.Point(468, 532);
+            this.right.Name = "right";
+            this.right.Size = new System.Drawing.Size(75, 41);
+            this.right.TabIndex = 6;
+            this.right.Text = ">>>";
+            this.right.UseVisualStyleBackColor = true;
+            this.right.Click += new System.EventHandler(this.right_Click);
+            // 
+            // automatic
+            // 
+            this.automatic.Location = new System.Drawing.Point(304, 532);
+            this.automatic.Name = "automatic";
+            this.automatic.Size = new System.Drawing.Size(75, 41);
+            this.automatic.TabIndex = 9;
+            this.automatic.Text = "|| / >";
+            this.automatic.UseVisualStyleBackColor = true;
+            this.automatic.Click += new System.EventHandler(this.automatic_Click);
+            // 
+            // left
+            // 
+            this.left.Location = new System.Drawing.Point(149, 532);
+            this.left.Name = "left";
+            this.left.Size = new System.Drawing.Size(75, 41);
+            this.left.TabIndex = 7;
+            this.left.Text = "<<<";
+            this.left.UseVisualStyleBackColor = true;
+            this.left.Click += new System.EventHandler(this.left_Click);
+            // 
+            // select
+            // 
+            this.select.Location = new System.Drawing.Point(66, 439);
+            this.select.Name = "select";
+            this.select.Size = new System.Drawing.Size(75, 45);
+            this.select.TabIndex = 8;
+            this.select.Text = "選擇";
+            this.select.UseVisualStyleBackColor = true;
+            this.select.Click += new System.EventHandler(this.select_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(174, 456);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 15);
+            this.label1.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(46, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(619, 386);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(728, 605);
+            this.Controls.Add(this.right);
+            this.Controls.Add(this.automatic);
+            this.Controls.Add(this.left);
+            this.Controls.Add(this.select);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "相片檢視器 Photo viewer";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button right;
+        private System.Windows.Forms.Button automatic;
+        private System.Windows.Forms.Button left;
+        private System.Windows.Forms.Button select;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Photo viewer/Photo viewer/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace Photo_viewer
+{
+    public partial class Form1 : Form
+    {
+        // 保存打開圖片的路徑
+        string imgPath = null;
+        Image newbitmap = null;
+        // 打開圖片的目錄
+        string directory = null;
+
+        // 目錄下的圖片集合
+        List<string> imgArray = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            left.Visible = false;
+            right.Visible = false;
+            automatic.Visible = false;
+
+        }
+
+        private void select_Click(object sender, System.EventArgs e)
+        {
+            using (OpenFileDialog fileDialog = new OpenFileDialog())
+            {
+                fileDialog.Filter = "圖片文件(*.jpg;*.bmp;*.png)|*.jpg;*.bmp;*.png|(All file(*.*)|*.*";
+                fileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+                if (fileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+                    imgPath = fileDialog.FileName;
+                    // 初始化圖片集合
+                    directory = Path.GetDirectoryName(imgPath);
+                    imgArray = ImageManager.GetImgCollection(directory);
+
+                    newbitmap = (Bitmap)Image.FromFile(imgPath);
+                    pictureBox1.Image = newbitmap;
+                }
+                left.Visible = true;
+                right.Visible = true;
+                automatic.Visible = true;
+
+                label1.Text = imgPath;  //顯示位置
+
+            }
+        }
+
+        private void right_Click(object sender, System.EventArgs e)
+        {
+            int index = GetIndex(imgPath);
+            // 釋放上一張圖片的资源，避免保存的时候出現ExternalException異常
+            newbitmap.Dispose();
+            if (index != imgArray.Count - 1)
+            {
+                SwitchImg(index + 1);
+            }
+            else
+            {
+                SwitchImg(0);
+            }
+            label1.Text = imgPath;  //顯示位置
+        }
+
+        private void left_Click(object sender, System.EventArgs e)
+        {
+            int index = GetIndex(imgPath);
+            // 釋放上一張圖片的资源，避免保存的时候出現ExternalException異常
+            newbitmap.Dispose();
+            if (index == 0)
+            {
+                SwitchImg(imgArray.Count - 1);
+            }
+            else
+            {
+                SwitchImg(index - 1);
+            }
+            label1.Text = imgPath;  //顯示位置
+        }
+        // 獲得打開圖片在圖片集合中的索引
+        private int GetIndex(string imagepath)
+        {
+            int index = 0;
+            for (int i = 0; i < imgArray.Count; i++)
+            {
+                if (imgArray[i].Equals(imagepath))
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            return index;
+        }
+
+        // 切换圖片的方法
+        private void SwitchImg(int index)
+        {
+            newbitmap = Image.FromFile(imgArray[index]);
+            pictureBox1.Image = newbitmap;
+            imgPath = imgArray[index];
+        }
+
+        public class ImageManager
+        {
+            // 獲得預覽圖片文件路徑下的圖片集合
+            public static List<string> GetImgCollection(string path)
+            {
+                string[] imgarray = Directory.GetFiles(path);
+                var result = from imgstring in imgarray
+                             where imgstring.EndsWith("jpg", StringComparison.OrdinalIgnoreCase) ||
+                             imgstring.EndsWith("png", StringComparison.OrdinalIgnoreCase) ||
+                             imgstring.EndsWith("bmp", StringComparison.OrdinalIgnoreCase)
+                             select imgstring;
+                return result.ToList();
+            }
+        }
+
+        private void automatic_Click(object sender, System.EventArgs e)
+        {
+            if (timer1.Enabled == false)
+            {
+                timer1.Enabled = true;
+            }
+            else
+            {
+                timer1.Enabled = false;
+            }      
+        }
+
+        private void timer1_Tick(object sender, System.EventArgs e)
+        {
+            right_Click(sender, e);
+        }
+
+
+    }
+}
